8d44ac6ae2be5d130cf34e1df8a7ad3b6d564063
B33blebroxx <B33blebroxx@gmail.com>
2024-12-04 17:18:33 -0600

    Converted multiple files to use the React Native Paper library, removed meal planning screen and tab from navigator. Made QoL changes and fixed alignment and spacing in several areas on the calculator screen as well as fixed 2 buttons with unintended behavior.

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { StatusBar, SafeAreaView, StyleSheet } from "react-native";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
+  useEffect(() => {
+    StatusBar.setBarStyle("light-content"); // Inverts text and icon colors to be visible on dark backgrounds
+    StatusBar.setBackgroundColor("#333"); // Set the status bar background to dark (or any color that fits your theme)
+  }, []);
+
   return (
-    <AppNavigator />
+    <PaperProvider>
+      <SafeAreaView style={styles.container}>
+        <AppNavigator />
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#333",
+  },
+});
